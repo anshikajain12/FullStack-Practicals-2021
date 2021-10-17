7f30414ad199dfdec6e9b9be9afaf42eaf2afac7
anshikajain12 <nisthajain2412@gmail.com>
2021-10-17 18:48:15 +0530

    update

--- JavaScript/assignmnet14.js
+++ JavaScript/assignmnet14.js
@@ -1,16 +1,13 @@
-let add = document.querySelector("#increament")
-let remove = document.querySelector("#decreament");
-
-let counter = document.querySelector("#number");
-let countervalue = 0;
-
-
-increment.addEventListener("click" , ()=>{
-    countervalue++;
-    counter.innerText = countervalue;
-})
-;
-decrement.addEventListener("click" , ()=>{
-    countervalue--;
-    counter.innerText = countervalue;
-});
+var data = 0;
+        document.getElementById("root").innerText=data;
+        function decrement(){
+            if(data == 0){
+                return;
+            }
+            data = data-1;
+            document.getElementById("root").innerText=data;
+        }
+        function increment(){
+            data = data+1;
+            document.getElementById("root").innerText=data;
+        }
