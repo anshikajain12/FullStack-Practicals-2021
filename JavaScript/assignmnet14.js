90163eb54563c1877d3007b1c1102807e9dc726d
anshikajain12 <nisthajain2412@gmail.com>
2021-10-17 09:04:58 +0530

    update

--- JavaScript/assignmnet14.js
+++ JavaScript/assignmnet14.js
@@ -1,19 +1,16 @@
-let add = document.getElementById('increament');
-let remove = document.getElementById('decreament');
+let add = document.querySelector("#increament")
+let remove = document.querySelector("#decreament");
 
-let int = document.getElementsById('number');
-let integer = 0;
+let counter = document.querySelector("#number");
+let countervalue = 0;
 
 
-function increase(){
-    integer= integer+1;
-    int.innerHTML = integer;
-}
-
-function decrease(){
-    integer-= 1;
-    int.innerHTML = integer;
-}
-
-add.addEventListener('click',increase());
-remove.addEventListener('click',decrease());
+increment.addEventListener("click" , ()=>{
+    countervalue++;
+    counter.innerText = countervalue;
+})
+;
+decrement.addEventListener("click" , ()=>{
+    countervalue--;
+    counter.innerText = countervalue;
+});
