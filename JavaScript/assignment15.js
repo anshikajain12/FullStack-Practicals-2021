e7ab52982ae1f8223f92a19ab9f755e043eec6b4
anshikajain12 <nisthajain2412@gmail.com>
2021-11-12 23:45:34 +0530

    update

---
+++ JavaScript/assignment15.js
@@ -0,0 +1,23 @@
+
+var emailId= document.getElementById("email");
+var pass1= document.getElementById("p1");
+var pass2= document.getElementById("p2");
+
+function name(){
+    var user = document.getElementById("username");
+    if(user.value==""){
+        alert("enter your valid name");
+    }
+    else{
+        
+    }
+}
+function email() {
+    
+}
+function password1() {
+    
+}
+function password2() {
+    
+}
